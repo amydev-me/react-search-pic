f3e9b99c0643277e912cf2d88a4b5ef6b4d0111e
Amy <ampyaephyonaing@gmail.com>
2019-07-30 21:39:12 +0630

    Invoking Callback in Children

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,17 +1,30 @@
 import React from 'react';
 
 
-const SearchBar = () => {
-    return (
-        <div className="ui segment">
-            <form className="ui form">
-                <div className="field">
-                    <label>Image Search</label>
-                    <input type="text" />
-                </div>
-            </form>
-        </div>
-    );
+class SearchBar extends React.Component {
+    state = { term: ''};
+
+    onFormSubmit = event =>{
+        event.preventDefault();
+
+        this.props.onFuck(this.state.term);
+    }
+
+    render(){
+        return (
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input 
+                            type="text" 
+                            value={ this.state.term } 
+                            onChange={e => this.setState({ term:e.target.value })}/>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default SearchBar;
