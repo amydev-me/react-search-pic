786073957bb6024eda63b1a64b677996415aaab1
Amy <ampyaephyonaing@gmail.com>
2019-08-08 16:53:21 +0630

    Remove Authorization KEY

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -5,6 +5,6 @@ export default axios.create({
     baseURL:'https://api.unsplash.com',
     headers:{
         Authorization:
-            'Client-ID 022cb25fd74077d7ed9e6aa37adccf16ff53a42cf4eadbe78b89bebdb4458bd4'
+            'Client-ID KEY'
     }
 })
